b226b17851fd308607e51c8961a5dfa224aad330
SrLuisito <agueromartinezluis9@gmail.com>
2023-07-12 00:24:41 -0300

    arreglando error en producs data

--- src/database/config/config.js
+++ src/database/config/config.js
@@ -2,21 +2,21 @@ module.exports = {
   "development": {
     "username": "root",
     "password": "asdf1234",
-    "database": "dbInstrumentosMusicalesDH",
+    "database": "gamezone",
     "host": "127.0.0.1",
     "dialect": "mysql"
   },
   "test": {
     "username": "root",
     "password": "asdf1234",
-    "database": "dbInstrumentosMusicalesDH",
+    "database": "gamezone",
     "host": "127.0.0.1",
     "dialect": "mysql"
   },
   "production": {
     "username": "root",
     "password": "asdf1234",
-    "database": "dbInstrumentosMusicalesDH",
+    "database": "gamezone",
     "host": "127.0.0.1",
     "dialect": "mysql"
   }
